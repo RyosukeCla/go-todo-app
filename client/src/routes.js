fc17014c16fe1d948301f40f816fcbd3187b82e5
RyosukeCla <ryosukeclarinet@gmail.com>
2018-10-19 05:53:14 +0900

    update routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,6 @@
 import IndexPage from './pages/index'
-import HiPage from './pages/hi'
 
 export default [
   { exact: true, path: '/', component: IndexPage },
-  { path: '/hi', component: HiPage }
 ]
 
